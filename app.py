f70a4eb41ba60ff7db28468a1c5f197a4b55d483
3oman <143084228+3oman@users.noreply.github.com>
2024-11-26 14:55:23 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api')
+def info():
+    id = request.args.get('id') 
+    token = request.args.get('token') 
+    mss = request.args.get('text')
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text={mss}
+    response = requests.post(url)
+    return response.json()
+if __name__ == "__main__":
+    app.run(debug=True)
