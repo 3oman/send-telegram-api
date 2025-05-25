db5afa5b024767f6a588b8b6fba2432eb8bce4ca
3oman <143084228+3oman@users.noreply.github.com>
2025-05-25 23:01:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,20 @@ def info():
     id = request.args.get('id') 
     token = request.args.get('token') 
     mss = request.args.get('text')
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text={mss}"
-    response = requests.post(url)
+    is_html = request.args.get('html')  # يتحقق إذا كان المستخدم يريد إرسال الرسالة بصيغة HTML
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    
+    payload = {
+        'chat_id': id,
+        'text': mss
+    }
+
+    if is_html == "1":
+        payload['parse_mode'] = 'HTML'
+
+    response = requests.post(url, data=payload)
     return response.json()
+
 if __name__ == "__main__":
     app.run(debug=True)
